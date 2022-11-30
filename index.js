c602095926f2be5da67c0c7c47ef91a2cb02c1fd
Salvador Salinas Serrano <sasalinas.dwfsc12@bootcamp.ucamp.io>
2022-11-30 01:44:36 -0600

    Se agrega el procesamiento de los datos para que sean mostrados correctamente en la tabla

--- index.js
+++ index.js
@@ -35,10 +35,8 @@ function buttonCapturar(){
 }
 
 function actualizaTabla()
-{    
-    let contHTML = "";
-    document.getElementById('tablaContenido').innerHTML = contHTML;
-
+{        
+    let contHTML = "";    
     contHTML = creaTablaDinamica(contHTML);
 
     document.getElementById('tablaContenido').innerHTML = contHTML;
@@ -49,7 +47,7 @@ function creaTablaDinamica(cadenaHTML)
     cadenaHTML += "<table class='table'>\
     <thead>\
       <tr>\
-        <th scope='col'>#</th>\
+        <th scope='col'>id</th>\
         <th scope='col'>Producto</th>\
         <th scope='col'>Descripción</th>\
         <th scope='col'>Modificar</th>\
@@ -69,15 +67,17 @@ function creaTablaDinamica(cadenaHTML)
 }
 
 
-function generaContenido(cadenaHTML)
+function generaContenido(contTableHTML)
 {
     
-    for(i=0; i < localStorage.length; i++)
-    {
-        cadenaHTML += " <tr>\
-        <th id='rowValue0' scope='row'>1</th>\
-        <td>Camisa</td>\
-        <td>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci laboriosam expedita tempora repudiandae veniam quasi voluptates! Corporis corrupti quae praesentium ad doloremque quis voluptate quas unde ut! Sed, corrupti dolore?</td>\
+    for(let i=0; i < localStorage.length; i++)
+    {        
+        let productoAux = JSON.parse(localStorage[i]);
+
+        contTableHTML += " <tr>\
+        <th id='rowValue0' scope='row'>"+i+"</th>\
+        <td>"+productoAux.nombre+"</td>\
+        <td>"+productoAux.descripcion+"</td>\
         <td class='click' id='modificar0' style='align-items: center;' onclick='detectaClick()'>\
             <svg id='modificar0' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>\
                 <path id='modificar0' d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>\
@@ -94,8 +94,12 @@ function generaContenido(cadenaHTML)
     </tr>"
     }
 
-    return cadenaHTML;
+    return contTableHTML;
 }
 
 
-window.onload(actualizaTabla())
+
+
+  window.addEventListener('load', function() {
+    actualizaTabla();
+});
